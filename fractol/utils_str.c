a85d5ffeaa8f5753fe5cec893b0407d3af7cc4bd
jugarcia <jugarcia42telefonica@gmail.com>
2025-06-23 04:54:29 +0200

    bonus-fix

--- fractol/utils_str.c
+++ fractol/utils_str.c
@@ -10,31 +10,6 @@ int ft_strcmp(const char *s1, const char *s2)
     return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 
-// Convierte string a int
-int ft_atoi(const char *str)
-{
-    int i = 0;
-    int sign = 1;
-    int result = 0;
-
-    while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
-        i++;
-
-    if (str[i] == '-' || str[i] == '+')
-    {
-        if (str[i] == '-')
-            sign = -1;
-        i++;
-    }
-
-    while (str[i] >= '0' && str[i] <= '9')
-    {
-        result = result * 10 + (str[i] - '0');
-        i++;
-    }
-    return (result * sign);
-}
-
 // Convierte string a double, sin exponentes, con signo y decimales
 static int skip_whitespace_and_sign(const char *str, int *sign)
 {
--- fractol/utils_str.h
+++ fractol/utils_str.h
@@ -2,7 +2,7 @@
 #define UTILS_STR_H
 
 int ft_strcmp(const char *s1, const char *s2);
-int ft_atoi(const char *str);
+static int skip_whitespace_and_sign(const char *str, int *sign);
 double ft_atof(const char *str);
 void ft_putstr(char *str);
 
