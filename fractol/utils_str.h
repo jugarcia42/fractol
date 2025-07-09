711a0e4a4ca999671590545ec3a9f1b5abc1099e
jugarcia42 <jugarcia42telefonica@gmail.com>
2025-07-09 18:05:07 +0200

    Delete fractol directory

--- fractol/utils_str.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTILS_STR_H
-#define UTILS_STR_H
-
-int ft_strcmp(const char *s1, const char *s2);
-static int skip_whitespace_and_sign(const char *str, int *sign);
-double ft_atof(const char *str);
-void ft_putstr(char *str);
-
-#endif
